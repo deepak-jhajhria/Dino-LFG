66803a7fd38d474998fc765526673d2cb252026c
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-02-19 09:16:18 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Index } from './pages/Index'
 import ScrollToTop from './components/ScrollToTop'
 import { usePreloader } from './components/Hooks'
 import Preloder from './components/Preloader'
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
-// import { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react'
 function App() {
-  // useEffect(() => { AOS.init({ once: true, }); }, [])
+  useEffect(() => { AOS.init({ once: true, }); }, [])
   const isLoading = usePreloader();
   return (
     <>
